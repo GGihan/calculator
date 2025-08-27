b9d43a75488b6790b4cb5d57c0db7efb6176983d
ggihan <140435642+GGihan@users.noreply.github.com>
2025-08-27 21:50:12 +0200

    Add event click on buttons and assign the input

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ function divide(a, b) {
     return a / b;
 }
 
-let sum;
-let firstNum;
-let secondNum;
-let operation;
+let sum = "";
+let firstNum = "";
+let secondNum = "";
+let operation = "";
 
 function operate(operator, firstNum, secondNum) {
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
     if (operator === '+') {
         sum = add(firstNum, secondNum);
     } else if (operator === '-') {
@@ -32,4 +34,48 @@ function operate(operator, firstNum, secondNum) {
     return sum;
 }
 
-console.log(operate('/', 4, 6));
+let allButtons = Array.from(document.querySelectorAll("button"));
+let display = document.querySelector("#display");
+let input;
+
+function clearInputs() {
+    firstNum = "";
+    secondNum = "";
+    operation = "";
+    input = "";
+    sum = "";
+}
+
+function assignInput(input) {
+    if (!isNaN(input)) {
+        if (!firstNum || !operation) {
+            firstNum += input;
+            operation = "";
+        } else {
+            secondNum += input;
+        }
+    } else {
+        if (input === "=" && (firstNum && secondNum && operation)) {
+            operate(operation, firstNum, secondNum);
+        } else if (input === "AC" || input === "CE") {
+            clearInputs();
+        } else if (input !== "=") {
+            operation = input;
+        };
+    };
+}
+
+function addEvent() {
+    for (let button of allButtons) {
+        button.addEventListener("click", () => {
+            input = button.textContent;
+            assignInput(input);
+            console.log(firstNum);
+            console.log(operation);
+            console.log(secondNum);
+            console.log(sum);
+        });
+    }
+}
+
+addEvent();
